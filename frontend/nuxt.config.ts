463c0305d15e44f57488bcebeca6038e831e8aee
Connor Smith <connorhsmith@gmail.com>
2026-06-02 01:13:29 +1000

    Add noindex while testing.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -14,6 +14,9 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en'
       },
+      meta: [
+        { name: 'robots', content: 'noindex, nofollow' } // TODO
+      ],
       link: [
         { rel: 'icon', type: 'image/png', href: '/icon.png' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@700;800;900&family=Quicksand:wght@400;500;600;700&display=swap' }
