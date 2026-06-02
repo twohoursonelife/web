e704b1949cdd7ac743bef38a3ec13b2535601ddb
Connor Smith <connorhsmith@gmail.com>
2026-06-02 22:54:43 +1000

    Mobile menu touch ups.

--- frontend/app/components/AppHeader.vue
+++ frontend/app/components/AppHeader.vue
@@ -32,14 +32,14 @@ watch(() => route.fullPath, () => {
       </div>
 
       <div class="md:hidden">
-        <UButton variant="ghost" :icon="isMobileMenuOpen ? 'i-lucide-x' : 'i-lucide-menu'" @click="isMobileMenuOpen = !isMobileMenuOpen" class="hover:text-brand-500" />
+        <UButton variant="ghost" :icon="isMobileMenuOpen ? 'i-lucide-x' : 'i-lucide-menu'" @click="isMobileMenuOpen = !isMobileMenuOpen" class="bg-zinc-800 hover:bg-zinc-700 w-10 h-10 p-0! text-brand-500! hover:text-brand-400!" />
       </div>
     </div>
     
     <div v-if="isMobileMenuOpen" class="md:hidden bg-zinc-800 absolute top-full left-0 right-0 border-t border-zinc-700 flex flex-col px-6 py-4 space-y-4 shadow-xl">
-      <NuxtLink v-for="link in links" :key="link.to" :to="link.to" class="block pb-2 border-b border-zinc-700 hover:text-brand-500">{{ link.label }}</NuxtLink>
+      <NuxtLink v-for="link in links" :key="link.to" :to="link.to" class="block pb-2 border-b border-zinc-700 text-center hover:text-brand-500">{{ link.label }}</NuxtLink>
       <UButton 
-        to="/download"
+        to="/first-time-playing"
         class="w-full mt-2"
       >
         Play Now
